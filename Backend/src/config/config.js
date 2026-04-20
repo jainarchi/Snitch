576fe05383609a7a7b13692d4fe3450b312ee758
jainarchi <imarchijain2405@gmail.com>
2026-04-20 23:43:34 +0530

    add google Oauth

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -11,10 +11,24 @@ if(! process.env.JWT_SECRET_KEY){
     throw new Error('JWT secret key not defined in environment variables')
 }
 
+if(! process.env.GOOGLE_CLIENT_ID){
+    throw new Error('Google client ID not defined in environment variables')
+}
+
+if(! process.env.GOOGLE_CLIENT_SECRET){
+    throw new Error('Google client secret not defined in environment variables')
+}
+
+
+
+
+
 
 export const config = {
     PORT: process.env.PORT || 3000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     MONGO_URI: process.env.MONGO_URI,
-    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
 }
